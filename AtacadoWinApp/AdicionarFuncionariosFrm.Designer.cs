566ce9614e3f2104912ddb606848b8e059e903cc
MatheusBritoO <matheusovelar77@gmail.com>
2022-06-09 17:19:18 -0400

    Atualizando o Forms, construindo formulario funcionario

---
+++ AtacadoWinApp/AdicionarFuncionariosFrm.Designer.cs
@@ -0,0 +1,416 @@
+﻿namespace AtacadoWinApp
+{
+    partial class AdicionarFuncionariosFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CodigoLbl = new System.Windows.Forms.Label();
+            this.NomeLbl = new System.Windows.Forms.Label();
+            this.SobrenomeLbl = new System.Windows.Forms.Label();
+            this.EnderecoLbl = new System.Windows.Forms.Label();
+            this.TelefoneLbl = new System.Windows.Forms.Label();
+            this.EmalLbl = new System.Windows.Forms.Label();
+            this.DataNacimentoLbl = new System.Windows.Forms.Label();
+            this.IdadeLbl = new System.Windows.Forms.Label();
+            this.AlturaLbl = new System.Windows.Forms.Label();
+            this.PesoLbl = new System.Windows.Forms.Label();
+            this.NacionalidadeLbl = new System.Windows.Forms.Label();
+            this.NaturalidadeLbl = new System.Windows.Forms.Label();
+            this.CpfLbl = new System.Windows.Forms.Label();
+            this.SexoLbl = new System.Windows.Forms.Label();
+            this.RgLbl = new System.Windows.Forms.Label();
+            this.CódigoTxt = new System.Windows.Forms.TextBox();
+            this.NomeTxt = new System.Windows.Forms.TextBox();
+            this.SobrenomeTxt = new System.Windows.Forms.TextBox();
+            this.EnderecoTxt = new System.Windows.Forms.TextBox();
+            this.TelefoneTxt = new System.Windows.Forms.TextBox();
+            this.EmailTxt = new System.Windows.Forms.TextBox();
+            this.DtNascimentoDtPicker = new System.Windows.Forms.DateTimePicker();
+            this.IdadeTxt = new System.Windows.Forms.TextBox();
+            this.AlturaTxt = new System.Windows.Forms.TextBox();
+            this.PesoTxt = new System.Windows.Forms.TextBox();
+            this.NacionalidadeTxt = new System.Windows.Forms.TextBox();
+            this.NaturalidadeTxt = new System.Windows.Forms.TextBox();
+            this.SexoCmb = new System.Windows.Forms.ComboBox();
+            this.RgTxt = new System.Windows.Forms.TextBox();
+            this.CpfTxt = new System.Windows.Forms.TextBox();
+            this.FecharBtn = new System.Windows.Forms.Button();
+            this.SalvarBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CodigoLbl
+            // 
+            this.CodigoLbl.AutoSize = true;
+            this.CodigoLbl.Location = new System.Drawing.Point(15, 6);
+            this.CodigoLbl.Name = "CodigoLbl";
+            this.CodigoLbl.Size = new System.Drawing.Size(49, 15);
+            this.CodigoLbl.TabIndex = 0;
+            this.CodigoLbl.Text = "Código:";
+            this.CodigoLbl.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NomeLbl
+            // 
+            this.NomeLbl.AutoSize = true;
+            this.NomeLbl.Location = new System.Drawing.Point(15, 35);
+            this.NomeLbl.Name = "NomeLbl";
+            this.NomeLbl.Size = new System.Drawing.Size(43, 15);
+            this.NomeLbl.TabIndex = 1;
+            this.NomeLbl.Text = "Nome:";
+            // 
+            // SobrenomeLbl
+            // 
+            this.SobrenomeLbl.AutoSize = true;
+            this.SobrenomeLbl.Location = new System.Drawing.Point(15, 64);
+            this.SobrenomeLbl.Name = "SobrenomeLbl";
+            this.SobrenomeLbl.Size = new System.Drawing.Size(71, 15);
+            this.SobrenomeLbl.TabIndex = 2;
+            this.SobrenomeLbl.Text = "Sobrenome:";
+            // 
+            // EnderecoLbl
+            // 
+            this.EnderecoLbl.AutoSize = true;
+            this.EnderecoLbl.Location = new System.Drawing.Point(15, 93);
+            this.EnderecoLbl.Name = "EnderecoLbl";
+            this.EnderecoLbl.Size = new System.Drawing.Size(59, 15);
+            this.EnderecoLbl.TabIndex = 3;
+            this.EnderecoLbl.Text = "Endereço:";
+            this.EnderecoLbl.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // TelefoneLbl
+            // 
+            this.TelefoneLbl.AutoSize = true;
+            this.TelefoneLbl.Location = new System.Drawing.Point(15, 122);
+            this.TelefoneLbl.Name = "TelefoneLbl";
+            this.TelefoneLbl.Size = new System.Drawing.Size(54, 15);
+            this.TelefoneLbl.TabIndex = 4;
+            this.TelefoneLbl.Text = "Telefone:";
+            // 
+            // EmalLbl
+            // 
+            this.EmalLbl.AutoSize = true;
+            this.EmalLbl.Location = new System.Drawing.Point(15, 154);
+            this.EmalLbl.Name = "EmalLbl";
+            this.EmalLbl.Size = new System.Drawing.Size(44, 15);
+            this.EmalLbl.TabIndex = 5;
+            this.EmalLbl.Text = "E-mail:";
+            this.EmalLbl.Click += new System.EventHandler(this.EmalLbl_Click);
+            // 
+            // DataNacimentoLbl
+            // 
+            this.DataNacimentoLbl.AutoSize = true;
+            this.DataNacimentoLbl.Location = new System.Drawing.Point(15, 181);
+            this.DataNacimentoLbl.Name = "DataNacimentoLbl";
+            this.DataNacimentoLbl.Size = new System.Drawing.Size(112, 15);
+            this.DataNacimentoLbl.TabIndex = 6;
+            this.DataNacimentoLbl.Text = "Data de Nacimento:";
+            // 
+            // IdadeLbl
+            // 
+            this.IdadeLbl.AutoSize = true;
+            this.IdadeLbl.Location = new System.Drawing.Point(373, 181);
+            this.IdadeLbl.Name = "IdadeLbl";
+            this.IdadeLbl.Size = new System.Drawing.Size(39, 15);
+            this.IdadeLbl.TabIndex = 7;
+            this.IdadeLbl.Text = "Idade:";
+            this.IdadeLbl.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // AlturaLbl
+            // 
+            this.AlturaLbl.AutoSize = true;
+            this.AlturaLbl.Location = new System.Drawing.Point(15, 210);
+            this.AlturaLbl.Name = "AlturaLbl";
+            this.AlturaLbl.Size = new System.Drawing.Size(42, 15);
+            this.AlturaLbl.TabIndex = 8;
+            this.AlturaLbl.Text = "Altura:";
+            // 
+            // PesoLbl
+            // 
+            this.PesoLbl.AutoSize = true;
+            this.PesoLbl.Location = new System.Drawing.Point(15, 239);
+            this.PesoLbl.Name = "PesoLbl";
+            this.PesoLbl.Size = new System.Drawing.Size(35, 15);
+            this.PesoLbl.TabIndex = 9;
+            this.PesoLbl.Text = "Peso:";
+            // 
+            // NacionalidadeLbl
+            // 
+            this.NacionalidadeLbl.AutoSize = true;
+            this.NacionalidadeLbl.Location = new System.Drawing.Point(15, 271);
+            this.NacionalidadeLbl.Name = "NacionalidadeLbl";
+            this.NacionalidadeLbl.Size = new System.Drawing.Size(86, 15);
+            this.NacionalidadeLbl.TabIndex = 10;
+            this.NacionalidadeLbl.Text = "Nacionalidade:";
+            this.NacionalidadeLbl.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // NaturalidadeLbl
+            // 
+            this.NaturalidadeLbl.AutoSize = true;
+            this.NaturalidadeLbl.Location = new System.Drawing.Point(15, 300);
+            this.NaturalidadeLbl.Name = "NaturalidadeLbl";
+            this.NaturalidadeLbl.Size = new System.Drawing.Size(78, 15);
+            this.NaturalidadeLbl.TabIndex = 11;
+            this.NaturalidadeLbl.Text = "Naturalidade:";
+            // 
+            // CpfLbl
+            // 
+            this.CpfLbl.AutoSize = true;
+            this.CpfLbl.Location = new System.Drawing.Point(15, 389);
+            this.CpfLbl.Name = "CpfLbl";
+            this.CpfLbl.Size = new System.Drawing.Size(31, 15);
+            this.CpfLbl.TabIndex = 12;
+            this.CpfLbl.Text = "CPF:";
+            // 
+            // SexoLbl
+            // 
+            this.SexoLbl.AutoSize = true;
+            this.SexoLbl.Location = new System.Drawing.Point(15, 331);
+            this.SexoLbl.Name = "SexoLbl";
+            this.SexoLbl.Size = new System.Drawing.Size(35, 15);
+            this.SexoLbl.TabIndex = 13;
+            this.SexoLbl.Text = "Sexo:";
+            // 
+            // RgLbl
+            // 
+            this.RgLbl.AutoSize = true;
+            this.RgLbl.Location = new System.Drawing.Point(15, 362);
+            this.RgLbl.Name = "RgLbl";
+            this.RgLbl.Size = new System.Drawing.Size(25, 15);
+            this.RgLbl.TabIndex = 14;
+            this.RgLbl.Text = "RG:";
+            // 
+            // CódigoTxt
+            // 
+            this.CódigoTxt.Location = new System.Drawing.Point(143, 6);
+            this.CódigoTxt.Name = "CódigoTxt";
+            this.CódigoTxt.ReadOnly = true;
+            this.CódigoTxt.Size = new System.Drawing.Size(100, 23);
+            this.CódigoTxt.TabIndex = 15;
+            // 
+            // NomeTxt
+            // 
+            this.NomeTxt.Location = new System.Drawing.Point(143, 35);
+            this.NomeTxt.Name = "NomeTxt";
+            this.NomeTxt.Size = new System.Drawing.Size(200, 23);
+            this.NomeTxt.TabIndex = 16;
+            // 
+            // SobrenomeTxt
+            // 
+            this.SobrenomeTxt.Location = new System.Drawing.Point(143, 64);
+            this.SobrenomeTxt.Name = "SobrenomeTxt";
+            this.SobrenomeTxt.Size = new System.Drawing.Size(200, 23);
+            this.SobrenomeTxt.TabIndex = 17;
+            // 
+            // EnderecoTxt
+            // 
+            this.EnderecoTxt.Location = new System.Drawing.Point(143, 93);
+            this.EnderecoTxt.Name = "EnderecoTxt";
+            this.EnderecoTxt.Size = new System.Drawing.Size(200, 23);
+            this.EnderecoTxt.TabIndex = 18;
+            // 
+            // TelefoneTxt
+            // 
+            this.TelefoneTxt.Location = new System.Drawing.Point(143, 122);
+            this.TelefoneTxt.Name = "TelefoneTxt";
+            this.TelefoneTxt.Size = new System.Drawing.Size(200, 23);
+            this.TelefoneTxt.TabIndex = 19;
+            // 
+            // EmailTxt
+            // 
+            this.EmailTxt.Location = new System.Drawing.Point(143, 151);
+            this.EmailTxt.Name = "EmailTxt";
+            this.EmailTxt.Size = new System.Drawing.Size(200, 23);
+            this.EmailTxt.TabIndex = 20;
+            // 
+            // DtNascimentoDtPicker
+            // 
+            this.DtNascimentoDtPicker.Location = new System.Drawing.Point(143, 181);
+            this.DtNascimentoDtPicker.Name = "DtNascimentoDtPicker";
+            this.DtNascimentoDtPicker.Size = new System.Drawing.Size(200, 23);
+            this.DtNascimentoDtPicker.TabIndex = 21;
+            this.DtNascimentoDtPicker.ValueChanged += new System.EventHandler(this.DtNascimentoDtPicker_ValueChanged);
+            // 
+            // IdadeTxt
+            // 
+            this.IdadeTxt.Location = new System.Drawing.Point(429, 181);
+            this.IdadeTxt.Name = "IdadeTxt";
+            this.IdadeTxt.Size = new System.Drawing.Size(46, 23);
+            this.IdadeTxt.TabIndex = 22;
+            // 
+            // AlturaTxt
+            // 
+            this.AlturaTxt.Location = new System.Drawing.Point(143, 210);
+            this.AlturaTxt.Name = "AlturaTxt";
+            this.AlturaTxt.Size = new System.Drawing.Size(100, 23);
+            this.AlturaTxt.TabIndex = 23;
+            // 
+            // PesoTxt
+            // 
+            this.PesoTxt.Location = new System.Drawing.Point(143, 239);
+            this.PesoTxt.Name = "PesoTxt";
+            this.PesoTxt.Size = new System.Drawing.Size(100, 23);
+            this.PesoTxt.TabIndex = 24;
+            // 
+            // NacionalidadeTxt
+            // 
+            this.NacionalidadeTxt.Location = new System.Drawing.Point(143, 271);
+            this.NacionalidadeTxt.Name = "NacionalidadeTxt";
+            this.NacionalidadeTxt.Size = new System.Drawing.Size(100, 23);
+            this.NacionalidadeTxt.TabIndex = 25;
+            // 
+            // NaturalidadeTxt
+            // 
+            this.NaturalidadeTxt.Location = new System.Drawing.Point(143, 300);
+            this.NaturalidadeTxt.Name = "NaturalidadeTxt";
+            this.NaturalidadeTxt.Size = new System.Drawing.Size(100, 23);
+            this.NaturalidadeTxt.TabIndex = 26;
+            // 
+            // SexoCmb
+            // 
+            this.SexoCmb.FormattingEnabled = true;
+            this.SexoCmb.Items.AddRange(new object[] {
+            "Masculino",
+            "Feminino"});
+            this.SexoCmb.Location = new System.Drawing.Point(143, 331);
+            this.SexoCmb.Name = "SexoCmb";
+            this.SexoCmb.Size = new System.Drawing.Size(121, 23);
+            this.SexoCmb.TabIndex = 27;
+            // 
+            // RgTxt
+            // 
+            this.RgTxt.Location = new System.Drawing.Point(143, 360);
+            this.RgTxt.Name = "RgTxt";
+            this.RgTxt.Size = new System.Drawing.Size(100, 23);
+            this.RgTxt.TabIndex = 28;
+            // 
+            // CpfTxt
+            // 
+            this.CpfTxt.Location = new System.Drawing.Point(143, 389);
+            this.CpfTxt.Name = "CpfTxt";
+            this.CpfTxt.Size = new System.Drawing.Size(100, 23);
+            this.CpfTxt.TabIndex = 29;
+            // 
+            // FecharBtn
+            // 
+            this.FecharBtn.Location = new System.Drawing.Point(675, 411);
+            this.FecharBtn.Name = "FecharBtn";
+            this.FecharBtn.Size = new System.Drawing.Size(75, 23);
+            this.FecharBtn.TabIndex = 30;
+            this.FecharBtn.Text = "Fechar";
+            this.FecharBtn.UseVisualStyleBackColor = true;
+            this.FecharBtn.Click += new System.EventHandler(this.FecharBtn_Click);
+            // 
+            // SalvarBtn
+            // 
+            this.SalvarBtn.Location = new System.Drawing.Point(675, 382);
+            this.SalvarBtn.Name = "SalvarBtn";
+            this.SalvarBtn.Size = new System.Drawing.Size(75, 23);
+            this.SalvarBtn.TabIndex = 31;
+            this.SalvarBtn.Text = "Salvar";
+            this.SalvarBtn.UseVisualStyleBackColor = true;
+            this.SalvarBtn.Click += new System.EventHandler(this.SalvarBtn_Click);
+            // 
+            // AdicionarFuncionariosFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SalvarBtn);
+            this.Controls.Add(this.FecharBtn);
+            this.Controls.Add(this.CpfTxt);
+            this.Controls.Add(this.RgTxt);
+            this.Controls.Add(this.SexoCmb);
+            this.Controls.Add(this.NaturalidadeTxt);
+            this.Controls.Add(this.NacionalidadeTxt);
+            this.Controls.Add(this.PesoTxt);
+            this.Controls.Add(this.AlturaTxt);
+            this.Controls.Add(this.IdadeTxt);
+            this.Controls.Add(this.DtNascimentoDtPicker);
+            this.Controls.Add(this.EmailTxt);
+            this.Controls.Add(this.TelefoneTxt);
+            this.Controls.Add(this.EnderecoTxt);
+            this.Controls.Add(this.SobrenomeTxt);
+            this.Controls.Add(this.NomeTxt);
+            this.Controls.Add(this.CódigoTxt);
+            this.Controls.Add(this.RgLbl);
+            this.Controls.Add(this.SexoLbl);
+            this.Controls.Add(this.CpfLbl);
+            this.Controls.Add(this.NaturalidadeLbl);
+            this.Controls.Add(this.NacionalidadeLbl);
+            this.Controls.Add(this.PesoLbl);
+            this.Controls.Add(this.AlturaLbl);
+            this.Controls.Add(this.IdadeLbl);
+            this.Controls.Add(this.DataNacimentoLbl);
+            this.Controls.Add(this.EmalLbl);
+            this.Controls.Add(this.TelefoneLbl);
+            this.Controls.Add(this.EnderecoLbl);
+            this.Controls.Add(this.SobrenomeLbl);
+            this.Controls.Add(this.NomeLbl);
+            this.Controls.Add(this.CodigoLbl);
+            this.Name = "AdicionarFuncionariosFrm";
+            this.Text = "Adicionar Funcionario";
+            this.Load += new System.EventHandler(this.AdicionarFuncionariosFrm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label CodigoLbl;
+        private Label NomeLbl;
+        private Label SobrenomeLbl;
+        private Label EnderecoLbl;
+        private Label TelefoneLbl;
+        private Label EmalLbl;
+        private Label DataNacimentoLbl;
+        private Label IdadeLbl;
+        private Label AlturaLbl;
+        private Label PesoLbl;
+        private Label NacionalidadeLbl;
+        private Label NaturalidadeLbl;
+        private Label CpfLbl;
+        private Label SexoLbl;
+        private Label RgLbl;
+        private TextBox CódigoTxt;
+        private TextBox NomeTxt;
+        private TextBox SobrenomeTxt;
+        private TextBox EnderecoTxt;
+        private TextBox TelefoneTxt;
+        private TextBox EmailTxt;
+        private DateTimePicker DtNascimentoDtPicker;
+        private TextBox IdadeTxt;
+        private TextBox AlturaTxt;
+        private TextBox PesoTxt;
+        private TextBox NacionalidadeTxt;
+        private TextBox NaturalidadeTxt;
+        private ComboBox SexoCmb;
+        private TextBox RgTxt;
+        private TextBox CpfTxt;
+        private Button FecharBtn;
+        private Button SalvarBtn;
+    }
+}
---
+++ AtacadoWinApp/AdicionarFuncionariosFrm.cs
@@ -0,0 +1,114 @@
+﻿using Atacado.POCO.RH;
+using Atacado.Service.RH;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AtacadoWinApp
+{
+    public partial class AdicionarFuncionariosFrm : Form
+    {
+        public AdicionarFuncionariosFrm()
+        {
+            InitializeComponent();
+            this.PreencherSexoCmb();
+        }
+
+        private void AdicionarFuncionariosFrm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmalLbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FecharBtn_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void SalvarBtn_Click(object sender, EventArgs e)
+        {
+            {
+                
+                FuncionarioPOCO poco = new FuncionarioPOCO()
+                {
+                    Cpf = CpfTxt.Text,
+                    Nome = NomeTxt.Text,
+                    SobreNome = SobrenomeTxt.Text,
+                    Endereco = EnderecoTxt.Text,
+                    Telefone = TelefoneTxt.Text,
+                    Email = EmailTxt.Text,
+                    Nacionalidade = NacionalidadeTxt.Text,
+                    Naturalidade = NaturalidadeTxt.Text,
+                    Rg = RgTxt.Text,
+                    DtNascimento = DtNascimentoDtPicker.Value,
+                    Sexo  = SexoCmb.SelectedValue.ToString()
+                
+                };
+
+                FuncionarioService srv = new FuncionarioService();
+                if (srv.Validar(poco) == false)
+                {
+                    string mensagem = srv.MensagensDeErro.Aggregate((x, y) => x + ";\n" + y);
+                    MessageBox.Show(mensagem, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    string mensagem = "CPF V�lido.";
+                    MessageBox.Show(mensagem, "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                return;
+            }
+        }
+
+        private void DtNascimentoDtPicker_ValueChanged(object sender, EventArgs e)
+        {
+          
+        }
+        private void PreencherSexoCmb()
+        {
+            SexoCmb.Items.Clear();
+
+            SexoCmb.DisplayMember = "Value";
+            SexoCmb.ValueMember = "Key";
+
+            Dictionary<string, string> items = new Dictionary<string, string>();
+            items.Add("M", "Masculino");
+            items.Add("F", "Feminino");
+
+            SexoCmb.DataSource = new BindingSource(items, null);
+            SexoCmb.SelectedIndex = 0;
+        }
+    
+    
+    }
+}
